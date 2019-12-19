20fef7b2d336b0ffb78a855c08a291c6b78eef51
PeetersCarl <42937083+PeetersCarl@users.noreply.github.com>
2019-12-19 10:51:10 +0100

    Add files via upload

---
+++ CloseCombat.h
@@ -0,0 +1,15 @@
+//
+// Created by Guido Frissaer on 05/12/2019.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_CLOSECOMBAT_H
+#define GBA_SPRITE_ENGINE_PROJECT_CLOSECOMBAT_H
+
+#include "Animal.h"
+
+class CloseCombat : public Animal {
+
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_CLOSECOMBAT_H
---
+++ Damage.h
@@ -0,0 +1,14 @@
+//
+// Created by Guido Frissaer on 05/12/2019.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_DAMAGE_H
+#define GBA_SPRITE_ENGINE_PROJECT_DAMAGE_H
+
+
+class Damage {
+
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_DAMAGE_H

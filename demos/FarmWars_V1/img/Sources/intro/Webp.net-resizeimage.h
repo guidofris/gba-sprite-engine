3ad5e08b8e803f8fb002be39049c9559d8f15cc8
PeetersCarl <carl.peeters@live.com>
2019-12-29 13:05:04 +0100

    all sources of images added

---
+++ demos/FarmWars_V1/img/Sources/intro/Webp.net-resizeimage.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(Webp_net_resizeimage)
+
+//======================================================================
+//
+//	Webp_net_resizeimage, 800x568@8, 
+//	+ palette 256 entries, not compressed
+//	+ 5 tiles (t|f|p reduced) not compressed
+//	+ regular map (flat), not compressed, 100x71 
+//	Total size: 512 + 320 + 14200 = 15032
+//
+//	Time-stamp: 2019-12-09, 13:53:56
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_WEBP_NET_RESIZEIMAGE_H
+#define GRIT_WEBP_NET_RESIZEIMAGE_H
+
+#define Webp_net_resizeimageTilesLen 320
+extern const unsigned short Webp_net_resizeimageTiles[160];
+
+#define Webp_net_resizeimageMapLen 14200
+extern const unsigned short Webp_net_resizeimageMap[7100];
+
+#define Webp_net_resizeimagePalLen 512
+extern const unsigned short Webp_net_resizeimagePal[256];
+
+#endif // GRIT_WEBP_NET_RESIZEIMAGE_H
+
+//}}BLOCK(Webp_net_resizeimage)
